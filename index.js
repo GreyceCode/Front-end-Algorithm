a7bbee66bf09410e74efd9852bd09f627615b601
Obafunmilayo Opeyemi Grace <oobafunmilayo@gmail.com>
2021-05-23 13:53:54 +0100

    new commit

--- index.js
+++ index.js
@@ -1,35 +1,32 @@
-function convertFarhToCelsius (farhTemp){
+function convertFahrToCelsius (farhTemp){
     if (typeof farhTemp === "number" || typeof farhTemp ==="string"){
         let C = ((farhTemp - 32) * 5) /9;
-        console.log(C.toFixed(4));
+        return C.toFixed(4)
         //console.log(`${farhTemp} is a valid number but a/an ${typeof farhTemp} type.`)
     }
     
     if (typeof farhTemp === "boolean") {
-        console.log(`${farhTemp} is not a valid number but a/an ${typeof farhTemp} type.`);
-        return `${farhTemp} is not a valid number but a/an ${typeof farhTemp} type.`;
+        console.log(`${farhTemp} is not a valid number but a/an ${typeof farhTemp}.`);
+        return `${farhTemp} is not a valid number but a/an ${typeof farhTemp}.`;
     }
 
     if(Array.isArray(farhTemp)) {
-        console.log(`[${farhTemp}] is not a valid number but a/an array type.`);
-        return `[${farhTemp}] is not a valid number but a/an array type.`;
+        console.log(`[${farhTemp}] is not a valid number but a/an array.`);
+        return `[${farhTemp}] is not a valid number but a/an array.`;
     }  else if (typeof farhTemp === "object") {
         console.log(
             `${JSON.stringify(
                 farhTemp            
-            )}  is not a valid number but a/an ${typeof farhTemp} type.`
+            )} is not a valid number but a/an ${typeof farhTemp}.`
         );
         return `${JSON.stringify(
             farhTemp            
-        )}  is not a valid number but a/an ${typeof farhTemp} type.`;
+        )} is not a valid number but a/an ${typeof farhTemp}.`;
     }
 
     let anyNumber = Number(farhTemp);
     if (Number.isNaN(anyNumber)) {
-        console.log(`${farhTemp} is not a valid number but a/an ${typeof farhTemp} type.`);
-        return `${farhTemp} is not a valid number but a/an ${typeof farhTemp} type.`;
+        console.log(`${farhTemp} is not a valid number but a/an ${typeof farhTemp}.`);
+        return `${farhTemp} is not a valid number but a/an ${typeof farhTemp}.`;
     }
 }
-
-convertFarhToCelsius("0");
-
--- main.js
+++ main.js
@@ -1,35 +1,31 @@
 function checkYuGiOh(n) {
+ 
+   if (isNaN(n)) {
+       return `invalid parameter: ${JSON.stringify(n)}`;
+       // return;
+   }
 
-    if (isNaN(n) === true) {
-        console.log(`invalid parameter: ${n}`);
-        return;
+   const arr = [];
+   for (let i = 1; i<= n; i++) {
+    if (i % 2 === 0 && i % 3 === 0 && i % 5 === 0) {
+       arr.push("yu-gi-oh");
+    } else if (i % 2 === 0 && i % 3 === 0) {
+       arr.push("yu-gi");
+    } else if (i % 2 === 0 && i % 5 === 0) {
+       arr.push("yu-oh");
+    } else if (i % 3 === 0 && i % 5 === 0) {
+       arr.push("gi-oh");
+    } else if (i % 2 === 0) {
+       arr.push("yu");
+    } else if (i % 3 === 0) {
+       arr.push("gi");
+    } else if (i % 5 === 0) {
+       arr.push("oh");
+    } else {
+       arr.push(i);
     }
 
-    const arr = [];
-    for (let i = 1; i<= n; i++) {
-     if (i % 2 === 0 && i % 3 === 0 && i % 5 === 0) {
-        arr.push("Yu-Gi-Oh");
-     } else if (i % 2 === 0 && i % 3 === 0) {
-        arr.push("Yu-Gi");
-     } else if (i % 2 === 0 && i % 5 === 0) {
-        arr.push("Yu-Oh");
-     } else if (i % 3 === 0 && i % 5 === 0) {
-        arr.push("Gi-Oh");
-     } else if (i % 2 === 0) {
-        arr.push("Yu");
-     } else if (i % 3 === 0) {
-        arr.push("Gi");
-     } else if (i % 5 === 0) {
-        arr.push("Oh");
-     } else {
-        arr.push(i);
-     }
 
-
-     console.log(arr);
-    }
+   }
+   return arr;
 }
-
-checkYuGiOh("5")
-checkYuGiOh("10")
-checkYuGiOh("fizzbuzz is meh")
